0a477691b353eb8c83deaf9de32c8b0b9741036f
Lavina Kathambi <50262369+LavinaKathambi@users.noreply.github.com>
2021-03-21 07:05:04 +0200

    Add files via upload

---
+++ TournamentWinner.py
@@ -0,0 +1,52 @@
+'''
+Tournament Winner:
+There's a tournament, and teams go head to head, a function takes in 2 arrays as input
+1. list of lists of which teams go against each other 
+2. an array that has the results 
+the len of both arrays are equeal hence, indexes correspond to each other 
+the goal is to find the winning team 
+'''
+
+# O(N) Time | O(K) Space
+def tournamentWinner(competitions, results):
+	currentBestTeam = "" # winner so far
+	scores =  {currentBestTeam:0}
+	
+
+	# loop through both arrays at the same time and keep track of results 
+	for indx, competition in enumerate(competitions):
+		result = results[indx]
+
+		homeTeam, awayTeam = competition
+
+		if result == 1:
+			winningTeam = homeTeam
+		else:
+			winningTeam = awayTeam
+
+	
+		# define another function to update scores in the hash table 
+		def scoresUpdate(teamName, pointsScored, scores):
+			if teamName not in scores:
+				scores[teamName] = 0
+
+			# add the points scored so far
+			scores[teamName] += pointsScored
+
+		scoresUpdate(winningTeam, 3, scores)
+
+		# check the current Best Team 
+		if scores[winningTeam] > scores[currentBestTeam]:
+			currentBestTeam = winningTeam
+	
+	return currentBestTeam
+
+
+
+arr = [["HTML", "C#"], ["C#", "Python"], ["Python", "HTML"]]
+resultz = [0, 0, 1]
+print(tournamentWinner(arr, resultz))
+
+
+
+
